6e5628fe07541fcb82481b18f86a8d7c3ce72853
Max Webb <webbwrites@gmail.com>
2019-04-06 11:53:07 -0600

    prototypes.js done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -72,8 +72,12 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
+const universities = []
+  for (let i=0; i < graduates.length; i++) {
+  universities.push(graduates[i].university)
+}
+
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
 
@@ -81,12 +85,19 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = []
+  for (i = 0; i < graduates.length; i++) {
+    contactInfo.push(graduates[i].first_name + " " + graduates[i].email)
+  };
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = []
+  for (i = 0; i < graduates.length; i++) {
+
+    };
+
 console.log(uni);
 
 
@@ -112,6 +123,13 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+
+zooAnimals.forEach(function(name) {
+  animalNames.push(zooAnimals.animal_name + " " + zooAnimals.scientific_name)
+});
+
+console.log(zooAnimals["animal_name"]);
+
 console.log(animalNames);
 
 /* Request 2: .map()
